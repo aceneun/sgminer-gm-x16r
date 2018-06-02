ac82048c7a181d55a9d98f874e7e3428319befc5
Vlad <vladlutiy@gmail.com>
2018-06-02 18:37:42 +0300

    Update compile script for linux

--- update_compile.sh
+++ update_compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo git fetch origin
 sudo git pull --rebase origin master
-sudo CFLAGS="-Os -Wall -march=native -I/opt/AMDAPPSDK-3.0/include" LDFLAGS="-L/opt/amdgpu-pro/lib/x86_64-linux-gnu" ./configure --disable-git-version --disable-adl --prefix=/opt/sgminer-1.0.0
-sudo make $(if $(THREADS="-j$(($(tail -c 2 /sys/devices/system/node/node0/cpulist 2>/dev/null)+2))"); then echo $THREADS; fi) 
+sudo CFLAGS="-O2 -Wall -march=native -std=gnu99" ./configure --disable-adl
+sudo make $(if $(THREADS="-j$(($(tail -c 2 /sys/devices/system/node/node0/cpulist 2>/dev/null)+2))"); then echo $THREADS; fi)
